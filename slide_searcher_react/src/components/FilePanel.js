430f74aa0645f4b17fc126cb6ac3783bed735126
pwlee <pwlee@pocl.io>
2021-07-30 16:32:38 +0900

    add input element

--- slide_searcher_react/src/components/FilePanel.js
+++ slide_searcher_react/src/components/FilePanel.js
@@ -4,14 +4,19 @@ function FilePanel() {
     const [video, setVideo] = useState();
     const [slide, setSlide] = useState();
 
-    const handleVideo = (e) => {setVideo(e.target.files[0]);}
-    const handleSlide = (e) => {setSlide(e.target.files[0]);}
-    const handleSubmit = () => { console.log(video); console.log(slide);}
+    const handleVideo = (e) => {
+        setVideo(e.target.files[0]);
+    }
+    const handleSlide = (e) => {
+        setSlide(e.target.files[0]);
+    }
+    const handleSubmit = () => {
+        // TODO handle submit
+    }
 
     return (<div>
-        <p>hi</p>
-        <input type="file" onClick = {handleVideo} name="Video File"/>
-        <input type="file" onClick = {handleSlide} name="Slide File"/>
+        <input type="file" accept="video/*" onChange = {handleVideo} name="Video File"/>
+        <input type="file" accept=".pdf,.ppt,.pptx" onChange = {handleSlide} name="Slide File"/>
         <button type="submit" onClick = {handleSubmit}>Submit</button>
     </div>);
 }
