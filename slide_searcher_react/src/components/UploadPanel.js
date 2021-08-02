37756f3151c08a10d55e86c32bba631507d84938
pwlee <pwlee@pocl.io>
2021-08-02 15:27:10 +0900

    delete unused state

--- slide_searcher_react/src/components/UploadPanel.js
+++ slide_searcher_react/src/components/UploadPanel.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 
 function UploadPanel({ onSubmit }) {
-    const [title, setTitle] = useState("");
-    const [video, setVideo] = useState();
-    const [slide, setSlide] = useState();
+    let title = "";
+    let video = undefined;
+    let slide = undefined;
 
     const handleTitle = (e) => {
-        setTitle(e.target.value);
+        title = e.target.value;
     }
     const handleVideo = (e) => {
-        setVideo(e.target.files[0]);
+        video = e.target.files[0];
     }
     const handleSlide = (e) => {
-        setSlide(e.target.files[0]);
+        slide = e.target.files[0];
     }
 
     return (<div>
