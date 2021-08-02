550111a1109bad1b96dde3619ebbfe3bc25021f1
nickjw0205 <nomadnick02@gmail.com>
2021-08-02 15:53:13 +0900

    add video stream and video index text

--- slide_searcher_react/src/App.js
+++ slide_searcher_react/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-video-js-player';
+import React, { Component } from 'react';
+import txt from './ch08_result.txt'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const gotTheText = txt;
+    return (
+      <>
+        <div className="video_player">
+          <ReactPlayer 
+            autostart autoPlay
+            src= "ppt_no_animated.mp4"
+            width="500px" 
+            height="500px" 
+            type="video/mp4" 
+            control={true} 
+          />
+        </div>
+        <div className="video_index">
+          pdf : ch8-annotated.pdf<br/>
+          video : ch8-video.mp4<br/>
+
+          start analyzation...
+        </div>
+      </>
+      );
+  }
 }
 
 export default App;
