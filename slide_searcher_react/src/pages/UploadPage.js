c177e4dce6e603834e4b8724b4800cbfdb405cd1
pwlee <pwlee@pocl.io>
2021-08-02 17:17:35 +0900

    add routing to main from upload

--- slide_searcher_react/src/pages/UploadPage.js
+++ slide_searcher_react/src/pages/UploadPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function UploadPage({ onSubmit }) {
     let title = "";
@@ -19,7 +20,7 @@ function UploadPage({ onSubmit }) {
         <input type="text" onChange = {handleTitle} name="Title"/>
         <input type="file" accept="video/*" onChange = {handleVideo} name="Video File"/>
         <input type="file" accept=".pdf,.ppt,.pptx" onChange = {handleSlide} name="Slide File"/>
-        <button type="submit" onClick = {() => onSubmit(title, video, slide)}>Submit</button>
+        <Link to="/" type="submit" onClick = {() => onSubmit(title, video, slide)}>Submit</Link>
     </div>);
 }
 
