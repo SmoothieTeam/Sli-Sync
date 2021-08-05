54f6caef2f641d5dc4da898358963f1a5e4901f6
nickjw0205 <nomadnick02@gmail.com>
2021-08-05 11:22:21 +0900

    add seek function

--- slide_searcher_react/src/App.js
+++ slide_searcher_react/src/App.js
@@ -1,33 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import ReactPlayer from 'react-video-js-player';
 import React, { Component } from 'react';
-import txt from './ch08_result.txt'
+import { render } from 'react-dom';
+import VideoIndex from './components/VideoIndex.js'
 
-class App extends Component {
-  render() {
-    const gotTheText = txt;
-    return (
-      <>
-        <div className="video_player">
-          <ReactPlayer 
-            autostart autoPlay
-            src= "ppt_no_animated.mp4"
-            width="500px" 
-            height="500px" 
-            type="video/mp4" 
-            control={true} 
-          />
-        </div>
-        <div className="video_index">
-          pdf : ch8-annotated.pdf<br/>
-          video : ch8-video.mp4<br/>
-
-          start analyzation...
-        </div>
-      </>
-      );
-  }
+function App(props) {
+  return(
+    <div className='App'>
+      <VideoIndex></VideoIndex>
+    </div>
+  );
 }
-
 export default App;
