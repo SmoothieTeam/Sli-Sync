d289f7961a09ebb783cc8bc44893171f9d40fcec
pwlee <pwlee@pocl.io>
2021-08-05 16:30:27 +0900

    add video slide edit feature

--- slide_searcher_react/src/pages/VideoEditPage.js
+++ slide_searcher_react/src/pages/VideoEditPage.js
@@ -1,12 +1,20 @@
 import ReactPlayer from 'react-player';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import SlideIndexEditor from '../components/SlideIndexEditor';
 
 function VideoEditPage({video: {source, name}, slideIndexes}) {
     const player = useRef(null);
-
+    const [currentSlideIndexes, setSlideIndexes] = useState(slideIndexes);
     const handleSeeking = (time) => {
         player.current.seekTo(time, 'seconds');
+    };
+    const handleChangeTime = (index, time) => {
+        console.log({index, time});
+        setSlideIndexes(prev => {
+            const newSlideIndexes = [...prev];
+            newSlideIndexes[index].time = time;
+            return newSlideIndexes;
+        });
     };
 
     return (<div>
@@ -23,7 +31,7 @@ function VideoEditPage({video: {source, name}, slideIndexes}) {
             />
         </div>
         <div className="video_index">
-            {slideIndexes.map(slideIndex => SlideIndexEditor({slideIndex, onSeek: handleSeeking}))}
+            {currentSlideIndexes.map((slideIndex, i) => SlideIndexEditor({slideIndex, onSeek: handleSeeking, onChange: (t) => handleChangeTime(i, t)}))}
         </div>
     </div>);
 }
