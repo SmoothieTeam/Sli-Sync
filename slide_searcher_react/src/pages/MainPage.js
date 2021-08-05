c774611ac050e825689130e8303abcf347ce2b52
nickjw0205 <nomadnick02@gmail.com>
2021-08-05 17:18:29 +0900

    add making link

--- slide_searcher_react/src/pages/MainPage.js
+++ slide_searcher_react/src/pages/MainPage.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 function MainPage({ slidedVideos }) {
     return (<div>
         <Link to='/upload'>Upload</Link>
-        <Link to='/videoview/a.mp4'>a</Link>
         { slidedVideos.map(({video}, index) => <li key={index}><Link to={'/videoview/' + video}>{video}</Link></li>) }
     </div>);
 }
