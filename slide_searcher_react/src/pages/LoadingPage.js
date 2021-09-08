d3bde64b8b5c1504e8eb0d2a5649074c32b70329
nickjw0205 <jwsuh0703@gmail.com>
2021-09-08 15:21:12 +0900

    draft of loading page

--- slide_searcher_react/src/pages/LoadingPage.js
+++ slide_searcher_react/src/pages/LoadingPage.js
@@ -4,16 +4,21 @@ import './LoadingPage.css';
 function UploadedPage() {
     return (<div>
         <div className='header'></div>
-        <div className='Loaing_Animation'>
+        <div className='loaing_animation'>
             <div className='circle'>
                 <img src='Loaing_Animation.png' className='done_image' />
             </div>
         </div>
+        <progress className='progress_bar' max="100" value="70"> 70% </progress>
         
-        <h2>DONE</h2>
-        Please check the results through the link below and get sharable link : ) <br/>
-        <button>URL</button> <button>Email</button> <br/>
-        <input /> <button>Share</button>
+        <div className='message'>
+            <h2>70%</h2>
+            <div className='guide_ment'>
+                It's <strong>Okay</strong> to move to another screen or exit window <br/>
+                while we set things up for you!
+            </div>
+        </div>
+        
     </div>);
 }
 
