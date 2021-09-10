5eaedb2e1b2f7a825898a53281cb60a0bb78d6dc
이평원 <pwlee@pocl.io>
2021-09-10 20:46:09 +0900

    add timeline frame

--- slide_searcher_react/src/components/SlideIndex.js
+++ slide_searcher_react/src/components/SlideIndex.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import './SlideIndex.css';
+
 function timeToString(timeInSecond) {
     return parseInt(timeInSecond / 60) + ':' + parseInt(timeInSecond % 60);
 }
 
-function SlideIndex({slideIndex: {index, time}, onSeek}) {
-    return (<div>
-        {index}: <button onClick={() => onSeek(time)}>{timeToString(time)}</button>
+function SlideIndex({className, slideIndex: {index, time}, onSeek}) {
+    return (<div className={className}>
+        <input className='slide_index_group' type="radio" name='slide_index_group' value={index} id={'slide_index_input_' + index}></input>
+        <label className='slide_index_input_label' htmlFor={'slide_index_input_' + index} onClick={() => onSeek(time)}>
+            <div className='index_container'>{index}</div>
+            <div className='timing_container'>{timeToString(time)}</div>
+        </label>
     </div>);
 }
 
--- slide_searcher_react/src/pages/VideoViewPage.js
+++ slide_searcher_react/src/pages/VideoViewPage.js
@@ -3,33 +3,37 @@ import React, { useRef } from 'react';
 import SlideIndex from '../components/SlideIndex';
 import { Link, useParams } from 'react-router-dom';
 
+import './VideoViewPage.css';
+
 function VideoViewPage({videoLoader, slideIndexLoader}) {
     const { id } = useParams();
     const player = useRef(null);
     const source = videoLoader.getSource(id);
     const title = videoLoader.getTitle(id);
     const slideIndexes = slideIndexLoader.getIndexes(id);
+
     const handleSeeking = (time) => {
         player.current.seekTo(time, 'seconds');
     };
 
-    return (<div>
-        <Link to='/'>UploadPages</Link> <tab/>
-        <Link to={'/edit/' + id}>VideoEdit</Link>
-        <h1>{title}</h1>
+    return (<div className='video_view_page'>
+        <div className="video_index">
+            <h1>{title}</h1>
+            {slideIndexes.map(slideIndex => SlideIndex({
+                className: 'slide_index_container', 
+                slideIndex, 
+                onSeek: handleSeeking
+            }))}
+        </div>
         <div className="video_player">
             <ReactPlayer 
-                className="video_player"
                 ref={player}
                 url= {source}
                 playing
                 controls
-                width="500px"
-                height="500px"
+                width='900px'
+                height='506px'
                 type="video/mp4" />
-        </div>
-        <div className="video_index">
-            {slideIndexes.map(slideIndex => SlideIndex({slideIndex, onSeek: handleSeeking}))}
         </div>
     </div>);
 }
