8fd42dbc5cd823eff2236838c7860cc3439a31ca
이평원 <pwlee@pocl.io>
2022-01-08 23:26:44 +0900

    add firebase dependencies

--- slide_searcher_react/src/App.js
+++ slide_searcher_react/src/App.js
@@ -2,22 +2,24 @@ import UploadPage from './pages/UploadPage.js';
 import { Switch, Route } from 'react-router-dom';
 import React from 'react';
 import VideoViewPage from './pages/VideoViewPage.js';
-import VideoEditPage from './pages/VideoEditPage.js';
 import UploadedPage from './pages/UploadedPage.js';
 import LoadingPage from './pages/LoadingPage.js';
 import HomePage from './pages/HomePage.js';
+import { initApp } from './firebase/FirebaseInit.js';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getStorage } from 'firebase/storage';
 
+
+const { app } = initApp();
+const firestore = getFirestore(app);
+const storage = getStorage(app);
 
 function App() {
-  const handleUpload = (t, v, s) => {
-    console.log(t);
-    console.log(v);
-    console.log(s);
+  const handleUpload = (title, video, slide) => {
+    console.log(title);
+    console.log(video);
+    console.log(slide);
   };
-  const handleUpdate = (v, s) => {
-    console.log(v);
-    console.log(s);
-  }
   const videoLoader = {
     getSource: (id) => 'ppt_no_animated.mp4',
     getTitle: (id) => '운영체제론 1강'
@@ -35,7 +37,7 @@ function App() {
   };
   const percentLoader = {
     getPercentage: (id) => 80
-  }
+  };
   const sendEmail = () => {};
   const copyLink = () => {};
   return(
@@ -45,12 +47,6 @@ function App() {
           <VideoViewPage
             videoLoader={videoLoader} 
             slideIndexLoader={slideIndexLoader}/>
-        </Route>
-        <Route exact path='/edit/:id'>
-          <VideoEditPage 
-            videoLoader={videoLoader} 
-            slideIndexLoader={slideIndexLoader}
-            onSubmit={handleUpdate}/>
         </Route>
         <Route path='/uploaded/:id'>
           <UploadedPage 
