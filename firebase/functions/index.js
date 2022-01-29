7e5302894382a03ac528c45e3da1155c1f0e5f6f
이평원 <pwlee@pocl.io>
2022-01-29 17:54:01 +0900

    add pub sub in firebase functions

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,13 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const { PubSub } = require('@google-cloud/pubsub');
+const projectId = 'videoslider-6cfed';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.createRequest = functions.firestore
+    .document('posts/{postId}')
+    .onCreate(async (snap, _) => {
+        const id = snap.id;
+        const json = { id };
+        const pubsub = new PubSub({ projectId });
+        await pubsub.topic('topic').publishMessage({ json });
+        return null;
+    });
